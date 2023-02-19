d4f846b0d50a4c6e290bf956ab7d04b9117d0473
Synergyst <na@example.com>
2023-02-19 05:05:21 -0600

    Add installer in hopes to fix crashing on systems without VS2022

--- mic_passthrough/MICPassthroughGUI/Properties/AssemblyInfo.cs
+++ mic_passthrough/MICPassthroughGUI/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("004bac68-d38c-4d94-8afa-cbb533193a8d")]
